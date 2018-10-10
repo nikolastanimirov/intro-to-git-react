42ae7c351cf9447cf3223923c7edaff831136244
Nick <nikeca17_kn@abv.bg>
2018-10-11 01:26:45 +0300

    first steps in the slider

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Slider from "./slider";
 import Navbar from "./nav-bar";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<Navbar />, document.getElementById("nav"));
+ReactDOM.render(<Slider />, document.getElementById("slider"));
