9ea86e60983aeb6f2c0e250d3189ab7340dca75e
Nick <nikeca17_kn@abv.bg>
2018-10-11 19:50:59 +0300

    fix for the slider, clears the css and adds Description module

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,16 +5,9 @@ import logo from "../src/resources/kibik_logo.png";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ul class="header_contents">
-          <li>
-            <img src={logo} class="logo" alt="logo" />
-          </li>
-          <li>
-            {" "}
-            <p class="name-tag">KIBIC Club</p>
-          </li>
-        </ul>
+      <div className="header clearfix">
+        <img src={logo} className="logo" alt="logo" />
+        <p className="name-tag">KIBIC Club</p>
       </div>
     );
   }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import Slider from "./slider";
 import Navbar from "./nav-bar";
+import Description from "./description";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<Navbar />, document.getElementById("nav"));
 ReactDOM.render(<Slider />, document.getElementById("slider"));
+ReactDOM.render(<Description />, document.getElementById("description"));
