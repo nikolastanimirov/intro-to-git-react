8bcb21adfd93e650d002a75103561735383f6646
Nick <nikeca17_kn@abv.bg>
2018-10-13 21:02:04 +0300

    adds builds and deployment

---
+++ react-app/src/header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import logo from "../src/resources/kibik_logo.png";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header clearfix">
+        <img src={logo} className="logo" alt="logo" />
+        <p className="name-tag">KIBIC Club</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Header from "./header.jsx";
 import Slider from "./slider";
 import Navbar from "./nav-bar";
 import Description from "./description";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <Header />
+    <Navbar />
+    <Slider />
+    <Description />
+  </div>,
+  document.getElementById("root")
+);
+
+/*
 ReactDOM.render(<Navbar />, document.getElementById("nav"));
 ReactDOM.render(<Slider />, document.getElementById("slider"));
 ReactDOM.render(<Description />, document.getElementById("description"));
+*/
